7355c386fc1ad37d1bea2e43f39360c1a47295a6
Ritesh Ranjan <ranjanritesh43@gmail.com>
2025-04-25 00:33:01 +0530

    variables,use-strict,data-types,type conversion, basic operators

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # dsa-realm
+
+I)(A to Z - Subrealm) -> Module
+
+A) Basics
+1) JS-Fundamentals
+i)Variables And Use Stricts:
--- src/a-to-z-subrealms/basics/js-fundamentals/js-fundamentals.js
+++ src/a-to-z-subrealms/basics/js-fundamentals/js-fundamentals.js
@@ -1,4 +1,95 @@
-// Js - fundamentals
+'use strict'
 
-//1.variables:
-// js variables
+// i) Variables and Use Strict:
+
+  //Use Strict:
+    let x = 10; // earlier it sets to global object -> window.x but after adding strict mode it will treates as. 
+    console.log(x)
+
+  //variables:
+    let message; // variable message -> declaration of variable.
+    message = 'sent data' //assigning value to variable.
+
+    let numberA = 1; // declaration and assigning the variable at same time.
+
+    const COlOR_RED = 'red color' // constants the value and variable which is constant in entire application which cannot be changed.
+
+// ii) Data types:
+
+   //js is dynamically types
+   let userName = 'Ritesh' // data type is string here. 
+   userName = 12 //data type is number.
+   console.log(userName)// from string data type changes to number so you can store any thing in the variable it doesnot matters if you changes it to other data type.
+
+   //Primitive Data Type: By Value
+   //1) Number : number
+   // regular number and special numeric values.
+   //Infinity, -Infinity, and NaN -> these are (special numeric values).
+
+   //2) BigInt: bigint
+   let bigInt = 1234434343n
+   console.log(typeof bigInt)
+
+   //3) String: string
+   let nameOfSheep = "corey";
+   let nameOfDog = 'jerry';
+   let goodFreinds = `${nameOfSheep} and ${nameOfDog} are really good freinds`;
+   console.log(goodFreinds);
+
+   //4) Boolean: boolean
+   let nameOfStudent = "Rakesh";
+   let age = 18;
+   console.log(`${nameOfStudent} has ${age > 20 } can drive`)
+
+   //5) Null 
+   let nameOfStudentA = null;
+   console.log(nameOfStudentA) // means empty or nothing is assigned to the variable.
+
+   //6) Undefined
+   let nameOfStudentB;
+   console.log(nameOfStudentB); // means the variable is just declared and its initial value is not assigned to it.
+
+   //Non-primitive Data Type : By Reference
+   let obj = {name: 'ritesh'}
+   console.log(obj.name)
+
+   //Type Of Operator:
+   console.log(typeof null)
+
+// iii) Type Conversions:
+   
+   //String Conversion -> converting values to string
+   console.log(typeof String(true), typeof String(NaN), typeof String(112121n));
+
+   //Number Conversion -> converting to numbers;
+   console.log(Number(''), Number(undefined), Number(NaN), Number('123234'), Number('123243Ritesg'));
+
+   //Boolean Conversion -> Converting Values to Numbers 
+   console.log(Boolean(undefined), Boolean(NaN), Boolean(''), Boolean(null), Boolean(1));
+
+// iv) Basic Operators and Maths:
+
+   //string Concatenation:
+   console.log(2+2+'1'+2+2, '4' - 1); // conversion to sting
+   console.log(+'1') // conversion to number 
+
+   // Assignment Operater:
+   let assignmentOperator = 2;
+   console.log(assignmentOperator);
+
+   let a,b,c;
+   a=b=c=assignmentOperator;//chaining assignment
+   console.log(a,b,c);
+
+   let n = 6;
+   n += 5 //-> n = n + 5;
+   console.log(n)
+
+   //Increment and Decrement:
+   // console.log(5++) -> only valid  in variables;
+   let counter = 2;
+   // counter++;
+   console.log(counter++); //returns old value i.e.;2
+   console.log(++counter); //returns new value 3
+
+   //Bitwise Operator: &&, ||, !
